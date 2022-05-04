dd9f9b245c230808dec71d51de22de0c0c507d12
aliceFeldhaus <alicesfeldhaus@gmail.com>
2022-05-04 19:28:07 -0300

    Aula usando heroku e PgAdimin como banco de dados, fizemos um CRUD basico

---
+++ Aula-04.05-conectandodb/index.js
@@ -0,0 +1,114 @@
+const express = require('express')
+const app = express()
+
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
+
+var pg = require('pg')
+
+var consString = "postgres://vxurljyvgwqkxo:c88c2abb47dac7ab3ee3d5ea45a9221f8ce32c5f49d94d87be8340aeee17e7a1@ec2-3-224-164-189.compute-1.amazonaws.com:5432/d9p4bqr2o6791r"
+
+const pool = new pg.Pool({ connectionString: consString, ssl: { rejectUnauthorized: false } })
+
+app.get('/', (req, res) => {
+    pool.connect((err, client) => {
+        if (err) {
+            return res.status(401).send('Conexão não autorizada')
+        }
+        res.status(200).send('conectado com sucesso')
+    })
+})
+
+/** create table usuarios (email varchar(50), senha varchar(200), perfil varchar(15)) */
+
+app.get('/criartabelausuario', (req, res) => {
+    pool.connect((err, client) => {
+        if (err) {
+            return res.status(401).send('Conexão não autorizada')
+        }
+        var sql = 'create table usuarios (email varchar(50), senha varchar(200), perfil varchar(15))'
+        client.query(sql, (error, result) => {
+            if (error) {
+                return res.status(401).send('Operação não autorizada')
+            }
+            res.status(200).send(result.rows)
+        })
+    })
+})
+
+/**insert into usuarios(email,senha,perfil)values($1,$2,$3) */
+/**[req.body.email,req.body.senha,req.body.perfil] */
+
+/**insert into usuarios(email,senha,perfil)
+ * values(req.body.email,req.body.senha,req.body.perfil) */
+
+app.post('/usuario', (req, res) => {
+    pool.connect((err, client) => {
+        if (err) {
+            return res.status(401).send('Conexão não autorizada')
+        }
+        client.query('select * from usuarios where email = $1', [req.body.email], (error, result) => {
+            if (error) {
+                return res.status(401).send('Operação não autorizada')
+            }
+
+            if (result.rowCount > 0) {
+                return res.status(200).send('Registro já existe')
+            }
+
+            var sql = 'insert into usuarios(email, senha, perfil) values ($1,$2,$3)'
+            client.query(sql, [req.body.email, req.body.senha, req.body.perfil], (error, result) => {
+                if (error) {
+                    return res.status(403).send('Operação não permitida')
+                }
+                res.status(201).send({
+                    mensagem: 'criado com sucesso',
+                    status: 201
+                })
+            })
+        })
+    })
+})
+
+app.get('/usuario', (req, res) => {
+    pool.connect((err, client) => {
+        if (err) {
+            return res.status(401).send("Conexão não autorizada")
+        }
+        client.query('select * from usuarios', (error, result) => {
+            if (error) {
+                return res.status(401).send('Operação não autorizada')
+            }
+            res.status(200).send(result.rows)
+        })
+    })
+})
+
+app.get('/usuario/:email', (req, res) => {
+    pool.connect((err, client) => {
+        if (err) {
+            return res.status(401).send("Conexão não autorizada")
+        }
+        client.query('select * from usuarios where email = $1', [req.params.email], (error, result) => {
+            if (error) {
+                return res.status(401).send('operação não permitida')
+            }
+            res.status(200).send(result.rows[0]) /**[{},{}]*/
+        })
+    })
+})
+app.delete('/usuario/:email', (req, res) => {
+    pool.connect((err, client) => {
+        if (err) {
+            return res.status(401).send('Conexão não autorizada')
+        }
+        client.query('delete from usuarios where email = $1', [req.params.email], (error, result) => {
+            if (error) {
+                return res.status(401).send('operação não autorizada')
+            }
+            res.status(200).send({ message: 'registro excluido com sucesso' })
+        })
+    })
+})
+
+app.listen(8081, () => console.log('Aplicação em execução na url http://localhost:8081'))
